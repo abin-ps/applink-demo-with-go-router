511d5eabf3003ca0f9cd7e8af6eac13557904bf9
Abin P S <abinps23@gmail.com>
2024-02-28 13:18:31 +0530

    categories assets updated

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -17,49 +17,53 @@ class Category {
       id: '1',
       name: 'Sports',
       description: 'Sports Category',
-      imageUrl: '',
+      imageUrl: 'https://w0.peakpx.com/wallpaper/560/470/HD-wallpaper-basketball-layup-sport-thumbnail.jpg',
     ),
     Category(
       id: '2',
       name: 'Racing',
       description: 'Racing Category',
-      imageUrl: '',
+      imageUrl:
+          'https://w0.peakpx.com/wallpaper/451/320/HD-wallpaper-racing-car-racing-car-sports-car-speed-thumbnail.jpg',
     ),
     Category(
       id: '3',
       name: 'Cycling',
       description: 'Cycling Category',
-      imageUrl: '',
+      imageUrl:
+          'https://w0.peakpx.com/wallpaper/356/906/HD-wallpaper-sick-downhill-mountain-bike-stunt-cycle-thumbnail.jpg',
     ),
     Category(
       id: '4',
       name: 'Jogging',
       description: 'Jogging Category',
-      imageUrl: '',
+      imageUrl: 'https://w0.peakpx.com/wallpaper/677/918/HD-wallpaper-jogging-athlete-morning-run-runner-thumbnail.jpg',
     ),
     Category(
       id: '5',
       name: 'Office',
       description: 'Office Category',
-      imageUrl: '',
+      imageUrl: 'https://w0.peakpx.com/wallpaper/304/531/HD-wallpaper-michel-scott-the-office-thumbnail.jpg',
     ),
     Category(
       id: '6',
       name: 'Playing',
       description: 'Playing Category',
-      imageUrl: '',
+      imageUrl:
+          'https://w0.peakpx.com/wallpaper/744/881/HD-wallpaper-ronaldo-with-messi-playing-chess-ronaldo-playing-chess-lionel-messi-sports-footballer-thumbnail.jpg',
     ),
     Category(
       id: '7',
       name: 'Public Speaking',
       description: 'Public Speaking Category',
-      imageUrl: '',
+      imageUrl: 'https://w0.peakpx.com/wallpaper/549/719/HD-wallpaper-man-speaking-on-stage-thumbnail.jpg',
     ),
     Category(
       id: '8',
       name: 'Drifting',
       description: 'Drifting Category',
-      imageUrl: '',
+      imageUrl:
+          'https://w0.peakpx.com/wallpaper/378/237/HD-wallpaper-formula-drift-pt2-tokyo-cars-formula-drift-car-bmw-drift-toyota-gt86-thumbnail.jpg',
     ),
   ];
 }
